c01c84bb57a85875d6d584b643964ff5f6c4d0ab
anandaandra <anandaandra09@gmail.com>
2022-01-07 22:56:58 +0800

    login mahasiswa

--- src/view/pages/auth/LoginMahasiswa.vue
+++ src/view/pages/auth/LoginMahasiswa.vue
@@ -137,8 +137,8 @@ export default {
     return {
       state: "signin",
       form: {
-        email: "master@mail.com",
-        password: "master"
+        email: "",
+        password: ""
       }
     };
   },
@@ -201,10 +201,14 @@ export default {
           }
         });
 
-        console.log(response);
-
-        if (response) {
-          // this.$router.push({ name: "dashboard" });
+        if (response.data && response.data.token) {
+          console.log(response.data);
+          localStorage.setItem("token", response.data.token);
+          localStorage.setItem("role", response.data.role);
+          localStorage.setItem("name", response.data.name);
+          localStorage.setItem("nim", response.data.nim);
+          localStorage.setItem("email", response.data.email);
+          this.$router.push({ name: "dashboard" });
         }
       } catch (err) {
         console.log(err.response.data.message);
